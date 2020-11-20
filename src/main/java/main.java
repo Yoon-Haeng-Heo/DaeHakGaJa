ff12f0e53ad1fd32c8acf74dc4c9d42efd2384c0
YoonHaeng Heo <submailid96@gmail.com>
2020-11-20 21:03:05 +0900

    add jdbc postgresql connect

--- src/main/java/main.java
+++
@@ -1,7 +0,0 @@
-public class main {
-    public static void main(String[] args){
-        System.out.println("hello");
-        home h = new home("this is test code");
-        System.out.println(h.getTest());
-    }
-}
