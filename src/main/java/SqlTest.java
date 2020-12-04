698421f2fef1cf55264e8ef5551ce3ff6e24eb1c
oohyun15 <sakiss@ajou.ac.kr>
2020-12-04 21:51:46 +0900

    Edit legend
    - majorSeq 컬럼 추가

--- src/main/java/SqlTest.java
+++ src/main/java/SqlTest.java
@@ -6,7 +6,6 @@ public class SqlTest
 {
     public static void main(String[] args) throws SQLException
     {
-        LoadAPI api = new LoadAPI("100394","290");
         home h = new home();
 
         try {
@@ -43,10 +42,11 @@ public class SqlTest
             Scanner scan = new Scanner(System.in);
             conn = DriverManager.getConnection(h.getUrl(), h.getUser(), h.getPassword());
             st = conn.createStatement();
-            System.out.println("Create relations");
 
+            // create table
+            System.out.println("Create relations");
             String sql = "create table chart(id bigint, major_id bigint, male_ratio decimal, female_ratio decimal, avg_salary integer, satisfaction decimal, employment_rate decimal, applicant_rate decimal);\n" +
-                    "create table legend(id bigint, name text);\n" +
+                    "create table legend(id bigint, name text, majorSeq bigint[]);\n" +
                     "create table major(id bigint, name text, summary text, main_subject text, job text, legend_id bigint, qualification text);\n" +
                     "create table university(id bigint, name text, area text);\n" +
                     "create table university_major(id bigint, university_id bigint, major_id bigint);";
@@ -55,15 +55,18 @@ public class SqlTest
 
             // insert legend
             System.out.println("Inserting tuples to Legend");
-            sql = "insert into legend values('100391', '인문계열');\n" +
-                    "insert into legend values('100392', '사회계열');\n" +
-                    "insert into legend values('100393', '교육계열');\n" +
-                    "insert into legend values('100394', '공학계열');\n" +
-                    "insert into legend values('100395', '자연계열');\n" +
-                    "insert into legend values('100396', '의약계열');\n" +
-                    "insert into legend values('100397', '예체능계열');";
+            sql = "insert into legend values('100391', '인문계열', '{29, 48, 56, 10145, 69, 93, 10053, 122, 124, 132, 134, 163, 164, 165, 166, 169, 10030, 10031, 179, 188}');\n" +
+                    "insert into legend values('100392', '사회계열', '{8, 22, 10139, 23, 24, 25, 26, 27, 45, 46, 50, 54, 10141, 10143, 70, 10148, 10007, 74, 373, 10026}');\n" +
+                    "insert into legend values('100393', '교육계열', '{10006, 44, 57, 58, 59, 68, 32, 108, 123, 174, 183, 225, 238, 263, 299, 10033, 331, 346, 350, 365}');\n" +
+                    "insert into legend values('100394', '공학계열', '{16, 17, 18, 20, 21, 30, 35, 36, 55, 10142, 61, 10147, 10142, 61, 10147, 10038, 79, 85, 86, 87, 88, 89, 290}');\n" +
+                    "insert into legend values('100395', '자연계열', '{7, 10144, 10146, 96, 105, 106, 111, 126, 10120, 177, 190, 245, 260, 261, 262, 10048, 265, 267, 296}');\n" +
+                    "insert into legend values('100396', '의약계열', '{9, 10, 43, 175, 176, 196, 10012, 210, 215, 255, 288, 341, 10016, 10046, 10045, 399, 10014, 402, 404, 405}');\n" +
+                    "insert into legend values('100397', '예체능계열', '{12, 11, 28, 10041, 33, 34, 37, 38, 39, 41, 49, 51, 10140, 67, 77, 78, 94, 113, 114, 120}');";
 
-            st.executeUpdate(sql);
+           st.executeUpdate(sql);
+
+            // load api
+            LoadAPI api = new LoadAPI("100394","290");
 
             // insert major
             System.out.println("Inserting tuples to Major");
@@ -92,3 +95,4 @@ public class SqlTest
         }
     }
 }
+
