0a9557e8ee5c1169a6679dcf10e01bd096ef2cc8
d-virusss <jyg0599@gmail.com>
2020-12-05 01:47:01 +0900

    make survey class

---
+++ src/main/java/survey.java
@@ -0,0 +1,23 @@
+public class survey {
+    public survey(){
+        String question_1 = "1. 대화 중 갈등이 생겼을 때 어떤식으로 해결하는 편인가요?";
+        String answer_1_1 = "a) 논리로 해결하려 한다.";
+        String answer_1_2 = "b) 공감을 통해 해결하려 한다.";
+
+        String question_2 = "2. 전공에 대해 어떤 생각을 가지고 계신가요?";
+        String answer_2_1 = "a) 전공을 반드시 살리는 학과에 가고 싶다.";
+        String answer_2_2 = "b) 전공을 굳이 살리지 않아도 괜찮다.";
+
+        String question_3 = "3. 어떤 것에 더 가치를 두시나요?";
+        String answer_3_1 = "a) 세상에 기여";
+        String answer_3_2 = "b) 나의 행복";
+
+        String question_4 = "4. 어떤 직업을 선택하고 싶으신가요?";
+        String answer_4_1 = "a) 불안정 할지라도 좋아하는 일";
+        String answer_4_2 = "b) 좋아하진 않더라도 안정적인 일";
+
+        String question_5 = "5. 취업을 한다면, 무엇이 더 중요한가요?";
+        String answer_5_1 = "a) 워라밸";
+        String answer_5_2 = "b) 임금";
+    }
+}
